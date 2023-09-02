7cc63bf05da1b742113d87d661d2841e3b3985c0
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-09-02 21:06:35 +0700

    204 - Create REST API to Send Message

---
+++ src/main/java/com/example/springbootkafka/controller/MessageController.java
@@ -0,0 +1,30 @@
+package com.example.springbootkafka.controller;
+
+import com.example.springbootkafka.kafka.KafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+
+    private KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    // http://localhost:8080/api/v1/kafka/publish?message=Hello%20World
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(
+            @RequestParam(name = "message")
+            String message
+    ) {
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to the topic");
+    }
+
+}
