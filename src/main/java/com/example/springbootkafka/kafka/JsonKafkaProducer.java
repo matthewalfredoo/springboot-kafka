d5c5a043e398ed3408478fb303e368177aab337b
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-09-02 22:05:27 +0700

    212 - Refactor Code to Externalize The Topic Name
    Remove all hard coded values

--- src/main/java/com/example/springbootkafka/kafka/JsonKafkaProducer.java
+++ src/main/java/com/example/springbootkafka/kafka/JsonKafkaProducer.java
@@ -3,6 +3,7 @@ package com.example.springbootkafka.kafka;
 import com.example.springbootkafka.payload.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JsonKafkaProducer {
+
+    @Value("${spring.kafka.topic-json.name}")
+    private String topicJsonName;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
 
@@ -27,7 +31,7 @@ public class JsonKafkaProducer {
 
         Message<User> message = MessageBuilder
                 .withPayload(data)
-                .setHeader(KafkaHeaders.TOPIC, "javaguides_json")
+                .setHeader(KafkaHeaders.TOPIC, topicJsonName)
                 .build();
 
         kafkaTemplate.send(message);
